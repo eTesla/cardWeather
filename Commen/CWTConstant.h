343bed16f55e10233d38f6543dc9fe3172fbe7b6
eTesla <easttesla@gmail.com>
2015-03-27 01:01:55 +0800

    fix UI

---
+++ Commen/CWTConstant.h
@@ -0,0 +1,13 @@
+//
+//  CWTConstant.h
+//  cardWeather
+//
+//  Created by April on 3/27/15.
+//  Copyright (c) 2015 east. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CWTConstant : NSObject
+
+@end
---
+++ Commen/CWTConstant.m
@@ -0,0 +1,13 @@
+//
+//  CWTConstant.m
+//  cardWeather
+//
+//  Created by April on 3/27/15.
+//  Copyright (c) 2015 east. All rights reserved.
+//
+
+#import "CWTConstant.h"
+
+@implementation CWTConstant
+
+@end
